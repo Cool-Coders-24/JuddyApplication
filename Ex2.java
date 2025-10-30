5b79b83e9366b208e629a39e5876e0540a921ac1
Cool-Coders-24 <coolcoders24@gmail.com>
2025-10-31 01:45:42 +0530

    Update Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -3,6 +3,9 @@ class  Ex1
 	static{
 		System.out.println("static block");
 	}
+	static {
+		System.out.println("hy i am juddy");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("main method");
